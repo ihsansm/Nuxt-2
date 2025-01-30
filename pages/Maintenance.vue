b4961c9cfd0bebb1f67f5aa6a872601a689bdf04
ihsan <ihsantwf301@gmail.com>
2025-01-30 22:03:38 +0700

    fixing layout, footer, hover effect, routing(maintenance)

---
+++ pages/Maintenance.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div class="main-content">
+        
+
+        <h1>Notes</h1>
+    <br>
+        <NotesNewNote/>
+    <br>
+    <br>
+    
+    <NotesNoteCard/>
+    
+    </div>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: 'default',
+    })
+    
+    const noteCard=resolveComponent('NotesNoteCard')
+    const newNote=resolveComponent('NotesNewNote')
+    
+    const boolean=ref(false)
+</script>
+
+<style>
+
+.main-content{
+    display: grid;
+    place-items: center;
+    grid-template-rows: auto;
+    padding: 1rem;
+    color: white;
+    text-decoration: none;
+}
+
+
+</style>
